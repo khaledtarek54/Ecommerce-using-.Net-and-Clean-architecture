7887a1a86bf9a95c708a658f8a6bf45e43e20631
abdelrahman-muhamed <abdelrahmanmuhammd@gmail.com>
2024-09-09 22:13:12 +0300

    adding and deduct in product inventory

--- Ecommerce.Application/Services/InventoryService.cs
+++ Ecommerce.Application/Services/InventoryService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Ecommerce.Application.Interfaces;
+using Ecommerce.Core.Entities;
+using Ecommerce.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,48 @@ using System.Threading.Tasks;
 
 namespace Ecommerce.Application.Services
 {
-    internal class InventoryService
+    public class InventoryService : IInventoryService
     {
+        private readonly IProductRepository _productRepository;
+
+        public InventoryService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task ProductDeduct(Order order)
+        {
+            foreach (var orderItem in order.OrderItems)
+            {
+
+                var product = _productRepository.GetProductByIdAsync(orderItem.ProductId);
+                if (product.IsCompletedSuccessfully)
+                {
+
+                    product.Result.StockQuantity -= orderItem.Quantity;
+
+
+                    await _productRepository.UpdateProductAsync(product.Result.Id, product.Result);
+                }
+            }
+        }
+
+
+        public async Task ProductRestock(Order order)
+        {
+            foreach (var orderItem in order.OrderItems)
+            {
+
+                var product = _productRepository.GetProductByIdAsync(orderItem.ProductId);
+                if (product.IsCompletedSuccessfully)
+                {
+
+                    product.Result.StockQuantity += orderItem.Quantity;
+
+                    await _productRepository.UpdateProductAsync(product.Result.Id, product.Result);
+
+                }
+            }
+        }
     }
 }
